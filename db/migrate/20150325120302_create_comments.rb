dd29acc2a5abce917edf4b10138d53ffb34705a9
studpad <for_business94@mail.ru>
2015-03-25 20:15:01 +0400

    change classes

--- db/migrate/20150325120302_create_comments.rb
+++ db/migrate/20150325120302_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :body
       t.belongs_to :user, index: true
-      t.belongs_to :commentable, polymorphic: true, index: tuer
+      t.belongs_to :commentable, polymorphic: true, index: true
 
       t.timestamps null: false
     end
