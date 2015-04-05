d1c8017b7f21c427ab9f08bea0377199b5899223
Никитин Максим Вадимович <ais-berg@mail.ru>
2015-04-05 16:59:17 +0400

    created design subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,6 +3,5 @@ class SubjectsController < ApplicationController
     @subjects = Subject.all
     @classroom = Classroom.find(params[:classroom_id])
     @news = Subject.find(params[:id]).news_items.where(classroom_id: params[:classroom_id])
-    render 'classrooms/show'
   end
 end
