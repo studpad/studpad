00cc909a1d066a035ba2c8ad1712e9d8a1a31049
Risent <bsstr@yandex.ru>
2015-04-05 12:14:47 +0300

    add useful methods for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,22 @@ class User < ActiveRecord::Base
     false
   end
 
+  def can_view?(classroom)
+    classroom.main_teacher_id == id || classroom_id == classroom.id
+  end
+
+  def main_teacher_of?(classroom)
+    teacher? && id == classroom.main_teacher_id
+  end
+
+  def teacher_of?(classroom)
+    teacher? && classroom.teachers.exists?(id)
+  end
+
+  def student_of?(classroom)
+    student? && classroom_id == classroom.id
+  end
+
   private
     def remove_whitespaces
       self.email = email.to_s.squish
