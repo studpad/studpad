115dd182cb09ae0d3367b0089f8904cf18d15ddd
Risent <bsstr@yandex.ru>
2015-04-18 02:39:40 +0300

    compile assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,15 @@ set :log_level, :debug
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/uploads}
 
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 namespace :deploy do
-
+  before :compile_assets, :migrate
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,8 @@
 
 set :stage, 'production'
 set :rails_env, 'production'
+set :migration_role, :db
+set :conditionally_migrate, true
 
 server "178.62.177.239", roles: %{web app db}, ssh_options: {
     user: "deploy",
