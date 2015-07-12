e7ff45a0fe3187490e4430aebbdba3ca455cab87
Risent <bsstr@yandex.ru>
2015-07-12 16:22:00 +0300

    Удалены ненужные файлы и выключена их генерация

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,26 @@ Bundler.require(*Rails.groups)
 
 module Studpad
   class Application < Rails::Application
-    config.time_zone = 'Europe/Moscow'
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  config.time_zone = 'Europe/Moscow'
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+  config.generators do |g|
+    g.test_framework  nil
+    g.assets false
+    g.helper false
+  end
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+  # config.time_zone = 'Central Time (US & Canada)'
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  # config.i18n.default_locale = :de
+
+  # Do not swallow errors in after_commit/after_rollback callbacks.
+  config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- test/controllers/classmates_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ClassmatesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
