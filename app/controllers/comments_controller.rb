ca6cf1cd823aef1071e8ee5d9d63bffba87f0ee4
Risent <bsstr@yandex.ru>
2015-07-15 20:01:49 +0300

    Исправлен баг при создании комментария

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def update
     c = Comment.find(params[:id])
-    c.update_attribute :body, params[:text]
+    c.update_attribute :text, params[:text]
     redirect_to classroom_path c.commentable.classroom.id
   end
 
