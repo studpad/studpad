446ec5abd1f2ebd5c27f6c8d8c2ce4bb64fd0492
Risent <bsstr@yandex.ru>
2015-07-17 22:46:01 +0300

    Редактирование данных пользователя и вывод его классов и сообществ на странице профиля

--- app/assets/javascripts/avatar.js
+++ app/assets/javascripts/avatar.js
@@ -35,35 +35,6 @@ $(document).on("page:load ready", function(){
         aspectRatio: 1
       });
     }
-
-      //Добавление превью файла
-    //   if (response.type == "image") {
-    //     html = "<div class = 'col-xs-6 set-of-photos4 show-X' style = 'background: url("
-    //     html += response.link
-    //     html += "); background-size: cover;'>\
-    //             <div class = 'del-this-file'>"
-    //     html += response.remove_link + "</div></div>"
-    //     $("#loaded-photos").append(html).find('a').click(function() {
-    //       $(this).parent().parent().remove()
-    //       input = $("input[name=attached_files]")
-    //       input.val(input.val().replace(response.id, ""))
-    //     })
-    //   } else {
-    //     html = "<div class = 'this-material this-material-preview show-X'>\
-    //                       <div class = 'del-this-file'>"
-    //     html += response.remove_link
-    //     html += "</div><div class = 'files-image type-document'></div><div class = 'name-files'>"
-    //     html += "<a>" + response.name + "</a>"
-    //     html += "</div></div>"
-    //     $("#loaded-documents").append(html).find('a').click(function() {
-    //       $(this).parent().parent().remove()
-    //       input = $("input[name=attached_files]")
-    //       input.val(input.val().replace(response.id, ""))
-    //     })
-    //   }
-    //   input = $("input[name=attached_files]")
-    //   files = input.val(input.val() + ' ' + response.id)
-    // }
   });
 
 });
