be4e467607b57ec53e2436a705700920798aef1a
Risent <bsstr@yandex.ru>
2015-07-19 15:05:21 +0300

    Исправлен баг с даными mailgun

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Studpad
   mail_config = YAML.load_file("#{Rails.root}/config/mailer.yml")[Rails.env]
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-          api_key: mail_config[:api_key],
-          domain: mail_config[:domain]
+          api_key: mail_config['api_key'],
+          domain: mail_config['domain']
   }
 
   # Settings in config/environments/* take precedence over those specified here.
