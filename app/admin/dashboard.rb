43a68009818f85fa0506e51b5c6b781457522110
Никитин Максим Вадимович <ais-berg@mail.ru>
2015-10-10 14:23:59 +0400

    инофрмер для метрикки

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,6 +4,19 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
+       column do
+        panel "Статистика" do
+          yandex_link = <<-LINK
+            <!-- Yandex.Metrika informer -->
+              <a href="https://metrika.yandex.ru/stat/?id=32955634&amp;from=informer"
+              target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/32955634/3_0_007FFFFF_007FFFFF_1_pageviews"
+              style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:32955634,lang:'ru'});return false}catch(e){}" /></a>
+            <!-- /Yandex.Metrika informer -->
+          LINK
+          yandex_link.html_safe
+        end
+      end
+
       column do
         panel "Пользователи" do
           h1 User.count
@@ -23,4 +36,5 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
   end # content
+
 end
