a8414e0f0e8729d3ea3da9d18f13284fa29a6a83
Никитин Максим Вадимович <ais-berg@mail.ru>
2015-10-20 20:49:07 +0300

    новый пустой аватар

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,6 +2,6 @@ class FeedController < ApplicationController
   skip_before_filter :require_login
 
   def show
-  	@materials = Material.all.order(created_at:)
+  	@materials = Material.all.order(created_at: :desc)
   end
 end
