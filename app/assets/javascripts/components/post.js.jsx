6b91d7dcd8e32493b3d0390a1518d7f93a01d97e
Никитин Максим Вадимович <ais-berg@mail.ru>
2015-11-05 14:41:08 +0300

    скрывать большое кол-во текста и возможнось показать полностью

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -108,7 +108,12 @@ const PostTextElementView = React.createClass({
       case ElementTypes.text:
         return (
           <div className = 'usual-post-text action-create-element-post'>
-            {this.props.element.text}
+            <div className = 'usual-post-text-text'>
+              {this.props.element.text}
+            </div>
+            <div className = 'usual-post-text-expand'>
+              <span className = 'usual-post-text-expand-span'>Показать полностью</span>
+            </div>
           </div>
         );
         break;
