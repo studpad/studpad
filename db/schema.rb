1bf45adaf41c4e159f134dde99bc0b3fc4b55e68
Risent <bsstr@yandex.ru>
2015-12-01 23:02:44 +0300

    hide notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130195924) do
+ActiveRecord::Schema.define(version: 20151201195533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,10 +210,11 @@ ActiveRecord::Schema.define(version: 20151130195924) do
     t.string   "text"
     t.boolean  "public",     default: true
     t.integer  "user_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "post_id"
     t.integer  "who_id"
+    t.boolean  "viewed",     default: false
   end
 
   add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
