0a1ee0d91a9350db3b214135e67b4250d28634dc
Никитин Максим Вадимович <ais-berg@mail.ru>
2016-01-01 15:44:32 +0300

    tags

--- app/assets/javascripts/components/post_text_management_panel.js.jsx
+++ app/assets/javascripts/components/post_text_management_panel.js.jsx
@@ -58,24 +58,6 @@ const PostTextManagmentPanel = React.createClass({
           </div>
           {tips}
         </div>
-        <div>
-          <div className='attached-files'>
-            <div className="post-type-material-item">
-              <div className="post-type-material-icon">
-              </div>
-              <div className="post-type-material-text">
-                <a href="/uploads/attachment/file/3928/10242690801923124.pdf" target="blank">10242690801923124...</a>
-              </div>
-            </div>
-            <div className="post-type-material-item">
-              <div className="post-type-material-icon">
-              </div>
-              <div className="post-type-material-text">
-                <a href="/uploads/attachment/file/3928/10242690801923124.pdf" target="blank">Красивая принцесса</a>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     );
   }
