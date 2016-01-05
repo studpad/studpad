0eb2cbc80b7e460a8565cfe79ef18c221a5b784a
Risent <bsstr@yandex.ru>
2016-01-05 22:46:31 +0300

    different upgrades

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105110730) do
+ActiveRecord::Schema.define(version: 20160105193613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,8 +162,9 @@ ActiveRecord::Schema.define(version: 20160105110730) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "active",     default: false
   end
 
   create_table "text_elements", force: :cascade do |t|
