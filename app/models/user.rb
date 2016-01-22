75dff9538023dea52aad819a5df9244336619b10
Никитин Максим Вадимович <ais-berg@mail.ru>
2016-01-22 23:13:09 +0300

    проверка

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ActiveRecord::Base
 
   def self.recommended_for(user)
     if user
-      where.not(id: [user.id] + user.all_follows.map(&:followable_id)).
+      where.not(id: [1,3,5,6,7]).
         joins(:posts).group('users.id').order('RANDOM()').limit(3)
     else
       all.limit(3)
