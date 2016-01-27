919d3f56506cbd604a949d1c376ff62409fe73df
Risent <bsstr@yandex.ru>
2016-01-27 19:37:13 +0300

    hotfix categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  skip_before_filter :require_login
+
   def show
     @posts = Category.find(params[:id]).posts.order(created_at: :desc).limit(params[:count])
     render 'posts/index', formats: :json
