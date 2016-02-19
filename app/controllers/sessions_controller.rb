eceba7851c7c75e83bb924b0aa6564e4b20033d7
Risent <bsstr@yandex.ru>
2016-02-20 00:24:11 +0300

    load image from vk

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,18 +21,27 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  # def vkontakte
-
-  # end
-
   def create_external
     result = request.env['omniauth.auth']
     name = result['info']['name']
     email = result['info']['email']
+    avatar_url = result['info']['image']
+    tmp_password = SecureRandom.hex 4
+
     u = User.find_or_create_by!(email: email) do |user|
+
+      tmp_file = Tempfile.new(['avatar', '.jpg'])
+      tmp_file.binmode
+
+      open(avatar_url) do |url_file|
+        tmp_file.write(url_file.read)
+      end
+      tmp_file.close
+      user.avatar = tmp_file
+
       user.name = name
-      user.password = "hello_world"
-      user.password_confirmation = 'hello_world'
+      user.password = tmp_password
+      user.password_confirmation = tmp_password
     end
     auto_login(u)
     redirect_to root_path
